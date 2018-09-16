9f4e27adaf5e986535c76731f86b701602678873
Sam Meech-Ward <sam@meech-ward.com>
2018-09-16 10:31:04 -0700

    Setup a unix socket server

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 
 #include <stdio.h>
+#include <errno.h>
+
+#include "smw_unix_server.h"
+
+void onConnect(SMWUnixServerSocket *socket) {
+  printf("Accepted a connection\n");
+}
+
+void onData(SMWUnixServerSocket *socket, char *data) {
+  printf("Data: %s\n", data);
+}
+
+void onClose(SMWUnixServerSocket *socket) {
+  printf("Connection Closed\n");
+}
 
 int main() {
+  SMWUnixServerSocket *socket;
+  SMWUnixServerSocketCreateError error = smw_unix_stream_socket_create("/tmp/sam6", &socket);
+  if (error != SMWUnixServerSocketConnectErrorNone) {
+    printf("Error %i %i", error, errno);
+    return 0;
+  }
+
+  smw_unix_stream_socket_accept_connections(socket, 100, onConnect, onData, onClose);
+
   printf("hello\n");
+
   return 0;
 }
---
+++ smw_unix_server.h
@@ -0,0 +1,7 @@
+#ifndef __smw_unix_server_h__
+#define __smw_unix_server_h__
+
+#include "smw_unix_server_socket_connect.h"
+#include "smw_unix_server_socket_create.h"
+
+#endif
