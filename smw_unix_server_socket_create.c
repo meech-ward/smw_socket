5b4d30a4051c1efa45ee0eb70650ddc39885cde9
Sam Meech-Ward <sam@meech-ward.com>
2018-09-16 10:30:11 -0700

    Add functions to create socket
    These handle the initial setup of the unix socket

---
+++ smw_unix_server_socket_create.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "smw_unix_server_socket_create.h"
+
+static const int backlog = 5;
+
+int smw_socket_file_descriptor() {
+  return socket(AF_UNIX, SOCK_STREAM, 0);
+}
+
+void smw_setup_socket_address(SocketAddress *socketAddress, const char *path) {
+  memset(socketAddress, 0, sizeof(SocketAddress));
+  
+  socketAddress->sun_family = AF_UNIX;
+  
+  strncpy(socketAddress->sun_path, path, sizeof(socketAddress->sun_path) - 1);
+}
+
+int smw_bind_socket(int fileDescriptor, SocketAddress *socketAddress) {
+  return bind(fileDescriptor, (struct sockaddr *) socketAddress, sizeof(SocketAddress));
+}
+
+SMWUnixServerSocketCreateError smw_unix_stream_socket_create(const char *filePath, SMWUnixServerSocket **s) {
+  SMWUnixServerSocket *socket = smw_unix_server_socket_malloc();
+
+  socket->_fileDescriptor = smw_socket_file_descriptor();
+  if (socket->_fileDescriptor == -1) {
+    return SMWUnixServerSocketCreateErrorCreatingSocket;
+  }
+
+  smw_setup_socket_address(socket->_unixSocket, filePath);
+
+  if (smw_bind_socket(socket->_fileDescriptor, socket->_unixSocket) == -1) {
+    return SMWUnixServerSocketCreateErrorBinding;
+  }
+  
+  if (listen(socket->_fileDescriptor, backlog) == -1) {
+    return SMWUnixServerSocketCreateErrorListening;
+  }
+  
+  *s = socket;
+
+  return SMWUnixServerSocketCreateErrorNone;
+}
+
---
+++ smw_unix_server_socket_create.h
@@ -0,0 +1,16 @@
+#ifndef __smw_unix_server_socket_create_h__
+#define __smw_unix_server_socket_create_h__
+
+#include "smw_unix_server_socket.h"
+
+typedef enum {
+  SMWUnixServerSocketCreateErrorNone = 0,
+  SMWUnixServerSocketCreateErrorCreatingSocket,
+  SMWUnixServerSocketCreateErrorBinding,
+  SMWUnixServerSocketCreateErrorAllocatingSocket,
+  SMWUnixServerSocketCreateErrorListening
+} SMWUnixServerSocketCreateError;
+
+SMWUnixServerSocketCreateError smw_unix_stream_socket_create(const char *filePath, SMWUnixServerSocket **s);
+
+#endif
